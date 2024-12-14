6f91504221aacef95f087877c90c07bdc4c5d518
boris.abramovich86 <bojaab3@gmail.com>
2024-12-14 16:20:20 +0200

    initial commit - add camera button

---
+++ app/src/main/java/com/example/bookshelf/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.bookshelf
+
+import android.content.Intent
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    private val CAMERA_REQUEST_CODE = 1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btnOpenCamera = findViewById<Button>(R.id.btnOpenCamera)
+        btnOpenCamera.setOnClickListener {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(intent, CAMERA_REQUEST_CODE)
+        }
+    }
+}
